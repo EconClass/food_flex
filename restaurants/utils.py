6c52ad33e0a71004d545e0cc8f5cb53f06c6c229
EconClass <bizmail327@gmail.com>
2019-06-29 20:02:48 -0700

    [add] save 10 script

--- restaurants/utils.py
+++ restaurants/utils.py
@@ -4,9 +4,13 @@ from .models import Restaurant
 
 
 def call_api_with_key(location="San Francisco"):
+    """This function searches for restaurants from Yelp's Fusion API
+    based on a provided location. It returns a JSON response converted
+    into a Python dictionary"""
+
     # Yelp authorization headers
     auth = {'Authorization': 'Bearer ' + os.getenv('YELP_KEY')}
-    url = 'https://api.yelp.com/v3/businesses/search?location='
+    url = 'https://api.yelp.com/v3/businesses/search?term=restaurants&location='
 
     try:
         # Request from Yelp Fusion API
@@ -20,6 +24,7 @@ def call_api_with_key(location="San Francisco"):
 
 
 def save_to_db(data={}):
+    """This function saves parsed JSON data (Python dictionaries) to the database"""
     if data == {}:
         raise ValueError('Cannot save nothing to database')
     
@@ -29,3 +34,19 @@ def save_to_db(data={}):
     restaurant.city = data['location']['address1']
 
     restaurant.save()
+
+
+def save_10(restaurants=[]):
+    """This function saves the first 10 elements in an array of a JSON response"""
+    if len(restaurants) <= 0:
+        raise ValueError('Cannot save empty data')
+
+    if len(restaurants) > 10:
+        restaurants[:10]
+    
+    for restaurant in restaurants:
+        save_to_db(restaurant)
+
+if __name__ == "__main__":
+    parsed = call_api_with_key()
+    save_10(parsed['businesses'])
