fa42f3ea1b78b247995d28331394deed06714527
daisukiyo <tk.rain09@gmail.com>
2019-06-29 20:52:57 -0700

    began accounts view/models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from restaurants.models import Restaurant
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(User):
+    vistedRestaurants = models.ManyToManyField(Restaurant)
+
+    def __str__(self):
+        return self.username
+ 
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .models import Account
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
+class ProfileView(tbd):
+    template_name = 'home.html' # check conditional render
+
+    def get_queryset(self):
+        """Return User Visited Restaurant"""
+        return Account.objects.order_by('name')[:5]
